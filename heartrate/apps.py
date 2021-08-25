4623a256fdff6c534f0ed44b70792572646b535c
ji-one <ds020228@naver.com>
2021-08-25 17:50:31 +0900

    feat: project initial setting

---
+++ heartrate/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class HeartrateConfig(AppConfig):
+    name = 'heartrate'
---
+++ vue.config.js
@@ -0,0 +1,26 @@
+// vue.config.js
+const BundleTracker = require("webpack-bundle-tracker");
+
+module.exports = {
+  publicPath: "http://127.0.0.1:8080/",
+  outputDir: "./dist/",
+
+  chainWebpack: (config) => {
+    config.optimization.splitChunks(false);
+
+    config
+      .plugin("BundleTracker")
+      .use(BundleTracker, [{ filename: "./webpack-stats.json" }]);
+
+    config.devServer
+      .public("http://0.0.0.0:8080")
+      .host("0.0.0.0")
+      .port(8080)
+      .https(false)
+      .headers({ "Access-Control-Allow-Origin": ["*"] });
+  },
+
+  pages: {
+    index: "src/main.js",
+  },
+};
