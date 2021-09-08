810e1347d35d1af64f01468878e94c6275ed20df
ji-one <ds020228@naver.com>
2021-09-08 13:39:57 +0900

    feat: create AverageHR model
    bpm이 저장될 때마다 시간대별 평균이 자동으로 생성되도록 함

--- heartrate/models.py
+++ heartrate/models.py
@@ -2,7 +2,15 @@ from django.db import models
 from accounts.models import User
 
 # Create your models here.
+
+
 class HR(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     bpm = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+class AverageHR(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    hour = models.IntegerField()
+    avg_bpm = models.IntegerField()
--- heartrate/views.py
+++ heartrate/views.py
@@ -2,17 +2,37 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.decorators import api_view
 from django.http import HttpResponse
-from heartrate.models import HR
+from heartrate.models import AverageHR, HR
 from accounts.models import User
+from django.db.models import Avg
 
 # Create your views here.
+
+
 @api_view(['POST'])
 def save_bpm(request):
     if not request.session.get('user_id'):
         return HttpResponse(status=401)
-    
+
     user_id = request.session.get('user_id')
-    user = User.objects.get(pk = user_id)
-    hr = HR(user = user, bpm = request.data.get('bpm'))
+    user = User.objects.get(pk=user_id)
+    hr = HR(user=user, bpm=request.data.get('bpm'))
     hr.save()
+    average(hr)
     return HttpResponse(status=201)
+
+
+def average(self, force_insert=False, force_update=False, *args, **kwargs):
+    super(HR, self).save(force_insert, force_update, *args, **kwargs)
+    if self.user:
+        avg_bpm = HR.objects.filter(
+            user=self.user, created_at__hour=self.created_at.hour).aggregate(Avg('bpm'))['bpm__avg']
+        avg_bpm = round(avg_bpm)
+
+        exist = AverageHR.objects.filter(
+            hour=self.created_at.hour, user=self.user)
+        if exist:
+            exist.update(avg_bpm=avg_bpm)
+        else:
+            AverageHR.objects.create(
+                hour=self.created_at.hour, user=self.user, avg_bpm=avg_bpm)
